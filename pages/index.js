793c6643346bf714c6dfc40a15815cf6005d06bb
Andrew Setyawan <34556683+AndrewSich@users.noreply.github.com>
2021-04-20 09:14:06 +0700

    Update index.js
    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,18 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import { Navbar } from '../components/Navbar'
+// import { Sidebar } from '../components/Sidebar'
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
         <title>Levxa - Global News Archives</title>
-        <link rel="icon" href="/fav.ico"/>
+        <link rel="icon" href="/components/navbar/brand.png"/>
       </Head>
 
       <Navbar />
+
 
       <main>
         <h1 className={styles.word}>Welcome to Levxa.com</h1>
