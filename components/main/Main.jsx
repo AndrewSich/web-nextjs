ee946db15be530b94c6c7435c3c7fd31bc12e090
Andrew Setyawan <34556683+AndrewSich@users.noreply.github.com>
2021-06-13 13:24:58 +0700

    Update Main.jsx
    Update final design components

--- components/main/Main.jsx
+++ components/main/Main.jsx
@@ -7,358 +7,89 @@ export const Main = () => {
 	<>
 		<main className={styles.main}>
 			<h1 className={styles.word}>Welcome to Levxa.com</h1>
-			<div className={styles.contentOne}>
-				<div className={styles.Featured}>
-					<h1 className={styles.contentOneTitle}>Featured</h1>
-					<ul className={styles.FeaturedContainer}>
+			<div className={styles.mainContent}>
+				<div className={styles.mCL}>
+					<div className={styles.latest}>
+						<h2 className={styles.contentTitle}>Latest Content</h2>
+						<div className={styles.cC}>
+							<ul className={styles.previewContainer}>
 
-						<li className={styles.FeaturedPreview}>
-							<a href='#'>
-								<div className={styles.FPContainer}>
-									<div className={styles.FPHead}>
-										<img src='../../wp-content/1.jpeg' />
-									</div>
-									<div className={styles.FPBody}>
-										<h2>Coinbase To Acquire Crypto Data Service Skew</h2>
-										<p>Coinbase, The Largest Cryptocurrency exchange in the US, is acquiring the...</p>
-										<div className={styles.featuredInfoContainer}>
-											<div className={styles.featuredInfoLeft}>
-												<b className={styles.FILAuthor}>By Andrew Setyawan</b>
-												<time>April 30, 2021</time>
-											</div>
-											<div className={styles.featuredInfoRight}>
-												<button className={styles.FIRButton}>read</button>
+								<li className={styles.preview}>
+									<a href="#">
+										<div className={styles.previewHead}>
+											<img src="../../wp-content/1.jpeg" alt="" />
+										</div>
+										<div className={styles.previewBody}>
+											<h2>The Chronical of Basic by Literals of Chronium Optical</h2>
+											<p>Bitcoin Is Cryptocurrency Based on C++ Programming Language Created By Satoshi Nakamoto On 2009 with Anouncement In StackOverFlow</p>
+											<div className={styles.pBDetail}>
+												<b>Andrew Setyawan</b>
+												<time>12 minute ago</time>
 											</div>
 										</div>
-									</div>
-								</div>
-							</a>
-						</li>
+									</a>
+								</li>
 
-						<li className={styles.FeaturedPreview}>
-							<a href='#'>
-								<div className={styles.FPContainer}>
-									<div className={styles.FPHead}>
-										<img src='../../wp-content/1.jpeg' />
-									</div>
-									<div className={styles.FPBody}>
-										<h2>Coinbase To Acquire Crypto Data Service Skew</h2>
-										<p>Coinbase, The Largest Cryptocurrency exchange in the US, is acquiring the...</p>
-										<div className={styles.featuredInfoContainer}>
-											<div className={styles.featuredInfoLeft}>
-												<b className={styles.FILAuthor}>By Andrew Setyawan</b>
-												<time>April 30, 2021</time>
-											</div>
-											<div className={styles.featuredInfoRight}>
-												<button className={styles.FIRButton}>read</button>
+								<li className={styles.preview}>
+									<a href="#">
+										<div className={styles.previewHead}>
+											<img src="../../wp-content/2.png" alt="" />
+										</div>
+										<div className={styles.previewBody}>
+											<h2>Ethereum Moves Toward ETH 2.0 With 'Super Experimental' Testnet</h2>
+											<p>Bitcoin Is Cryptocurrency Based on C++ Engine Language Created By Satoshi Nakamoto On 2009 with Anouncement In StackOverFlow</p>
+											<div className={styles.pBInfo}>
+												<div className={styles.pBILeft}>
+													<b>Andrew Setyawan Sulaimander</b>
+													<time>1 minute ago</time>
+												</div>
+												<button className={styles.readBtn}>read</button>
 											</div>
 										</div>
-									</div>
-								</div>
-							</a>
-						</li>
+									</a>
+								</li>
 
-						<li className={styles.FeaturedPreview}>
-							<a href='#'>
-								<div className={styles.FPContainer}>
-									<div className={styles.FPHead}>
-										<img src='../../wp-content/1.jpeg' />
-									</div>
-									<div className={styles.FPBody}>
-										<h2>Coinbase To Acquire Crypto Data Service Skew</h2>
-										<p>Coinbase, The Largest Cryptocurrency exchange in the US, is acquiring the...</p>
-										<div className={styles.featuredInfoContainer}>
-											<div className={styles.featuredInfoLeft}>
-												<b className={styles.FILAuthor}>By Andrew Setyawan</b>
-												<time>April 30, 2021</time>
-											</div>
-											<div className={styles.featuredInfoRight}>
-												<button className={styles.FIRButton}>read</button>
-											</div>
+								<li className={styles.preview}>
+									<a href="#">
+										<div className={styles.previewHead}>
+											<img src="../../wp-content/3.jpg" alt="" />
 										</div>
-									</div>
-								</div>
-							</a>
-						</li>
-
-						<li className={styles.FeaturedPreview}>
-							<a href='#'>
-								<div className={styles.FPContainer}>
-									<div className={styles.FPHead}>
-										<img src='../../wp-content/1.jpeg' />
-									</div>
-									<div className={styles.FPBody}>
-										<h2>Coinbase To Acquire Crypto Data Service Skew</h2>
-										<p>Coinbase, The Largest Cryptocurrency exchange in the US, is acquiring the...</p>
-										<div className={styles.featuredInfoContainer}>
-											<div className={styles.featuredInfoLeft}>
-												<b className={styles.FILAuthor}>By Andrew Setyawan</b>
-												<time>April 30, 2021</time>
-											</div>
-											<div className={styles.featuredInfoRight}>
-												<button className={styles.FIRButton}>read</button>
-											</div>
+										<div className={styles.previewBody}>
+											<h2>Coinbase To Acquire Crypto Data Service SkewCoinbase To Acquires</h2>
 										</div>
-									</div>
-								</div>
-							</a>
-						</li>
+									</a>
+								</li>
 
-						<li className={styles.FeaturedPreview}>
-							<a href='#'>
-								<div className={styles.FPContainer}>
-									<div className={styles.FPHead}>
-										<img src='../../wp-content/1.jpeg' />
-									</div>
-									<div className={styles.FPBody}>
-										<h2>Coinbase To Acquire Crypto Data Service Skew</h2>
-										<p>Coinbase, The Largest Cryptocurrency exchange in the US, is acquiring the...</p>
-										<div className={styles.featuredInfoContainer}>
-											<div className={styles.featuredInfoLeft}>
-												<b className={styles.FILAuthor}>By Andrew Setyawan</b>
-												<time>April 30, 2021</time>
-											</div>
-											<div className={styles.featuredInfoRight}>
-												<button className={styles.FIRButton}>read</button>
-											</div>
+								<li className={styles.preview}>
+									<a href="#">
+										<div className={styles.previewHead}>
+											<img src="../../wp-content/4.jpg" alt="" />
 										</div>
-									</div>
-								</div>
-							</a>
-						</li>
+										<div className={styles.previewBody}>
+											<h2>The Chronical of Basic by Literals of Chronium Optical Base Engine</h2>
+										</div>
+									</a>
+								</li>
 
-					</ul>
+								<li className={styles.preview}>
+									<a href="#">
+										<div className={styles.previewHead}>
+											<img src="../../wp-content/5.jpg" alt="" />
+										</div>
+										<div className={styles.previewBody}>
+											<h2>Ethereum moves Toward ETH 2.0 With 'Super Experiental' Testnet</h2>
+										</div>
+									</a>
+								</li>
+
+							</ul>
+						</div>
+					</div>
 				</div>
-
-				<div className={styles.Popular}>
-					<h1 className={styles.contentOneTitle}>Popular</h1>
-					<ul className={styles.PopularContainer}>
-						
-						<li className={styles.PopularPreview}>
-							<a href='#'>
-								<div className={styles.PPContainer}>
-									<div className={styles.PPHead}>
-										<img src='../../wp-content/1.jpeg' alt='bitcoin_world' />
-									</div>
-									<div className={styles.PPBody}>
-										<h2>Ethereum Moves Toward ETH 2.0 With 'Super Experimental' Testnet</h2>
-										<p>Coinbase, The Largest Cryptocurrency exchange in the US, is acquiring the...</p>
-										<div className={styles.popularInfoContainer}>
-											<div className={styles.popularInfoLeft}>
-												<b className={styles.PILAuthor}>By Defyna Permatasari</b>
-												<time>Mei 1, 2021</time>
-											</div>
-											<div className={styles.popularInfoRight}>
-												<button className={styles.PIRButton}>read</button>
-											</div>
-										</div>
-									</div>
-								</div>
-							</a>
-						</li>
-
-						<li className={styles.PopularPreview}>
-							<a href='#'>
-								<div className={styles.PPContainer}>
-									<div className={styles.PPHead}>
-										<img src='../../wp-content/2.png' alt='bitcoin_world' />
-									</div>
-									<div className={styles.PPBody}>
-										<h2>Turkish Exchange Thodex Sent $125M in Bitcoin to Kraken Before Closing: Report</h2>
-										<p>Coinbase, The Largest Cryptocurrency exchange in the US, is acquiring the...</p>
-										<div className={styles.popularInfoContainer}>
-											<div className={styles.popularInfoLeft}>
-												<b className={styles.PILAuthor}>By Andrew Setyawan</b>
-												<time>Mei 1, 2021</time>
-											</div>
-											<div className={styles.popularInfoRight}>
-												<button className={styles.PIRButton}>read</button>
-											</div>
-										</div>
-									</div>
-								</div>
-							</a>
-						</li>
-
-						<li className={styles.PopularPreview}>
-							<a href='#'>
-								<div className={styles.PPContainer}>
-									<div className={styles.PPHead}>
-										<img src='../../wp-content/3.jpg' alt='bitcoin_world' />
-									</div>
-									<div className={styles.PPBody}>
-										<h2>Ripple's Radical Call for Greener Bitcoin</h2>
-										<p>Coinbase, The Largest Cryptocurrency exchange in the US, is acquiring the...</p>
-										<div className={styles.popularInfoContainer}>
-											<div className={styles.popularInfoLeft}>
-												<b className={styles.PILAuthor}>By Anik Handayani</b>
-												<time>Mei 1, 2021</time>
-											</div>
-											<div className={styles.popularInfoRight}>
-												<button className={styles.PIRButton}>read</button>
-											</div>
-										</div>
-									</div>
-								</div>
-							</a>
-						</li>
-
-						<li className={styles.PopularPreview}>
-							<a href='#'>
-								<div className={styles.PPContainer}>
-									<div className={styles.PPHead}>
-										<img src='../../wp-content/4.jpg' alt='bitcoin_world' />
-									</div>
-									<div className={styles.PPBody}>
-										<h2>Ethereum Moves Toward ETH 2.0 With 'Super Experimental' Testnet</h2>
-										<p>Coinbase, The Largest Cryptocurrency exchange in the US, is acquiring the...</p>
-										<div className={styles.popularInfoContainer}>
-											<div className={styles.popularInfoLeft}>
-												<b className={styles.PILAuthor}>By Defyra Kikiasari</b>
-												<time>Mei 1, 2021</time>
-											</div>
-											<div className={styles.popularInfoRight}>
-												<button className={styles.PIRButton}>read</button>
-											</div>
-										</div>
-									</div>
-								</div>
-							</a>
-						</li>
-
-						<li className={styles.PopularPreview}>
-							<a href='#'>
-								<div className={styles.PPContainer}>
-									<div className={styles.PPHead}>
-										<img src='../../wp-content/5.jpg' alt='bitcoin_world' />
-									</div>
-									<div className={styles.PPBody}>
-										<h2>Ethereum Moves Toward ETH 2.0 With 'Super Experimental' Testnet</h2>
-										<p>Coinbase, The Largest Cryptocurrency exchange in the US, is acquiring the...</p>
-										<div className={styles.popularInfoContainer}>
-											<div className={styles.popularInfoLeft}>
-												<b className={styles.PILAuthor}>Defyna Permatasari</b>
-												<time>Mei 1, 2021</time>
-											</div>
-											<div className={styles.popularInfoRight}>
-												<button className={styles.PIRButton}>read</button>
-											</div>
-										</div>
-									</div>
-								</div>
-							</a>
-						</li>
-
-					</ul>
+				<div className={styles.mCR}>
+					<h2 className={styles.contentTitle}>Top Content</h2>
 				</div>
 			</div>
-
-			<div className={styles.contentTwo}>
-				<div className={styles.recommend}>
-					<h1 className={styles.contentTwoTitle}>Recommend</h1>
-					<ul className={styles.recommendContainer}>
-
-						<li className={styles.recommendPreview}>
-							<a href="#">
-								<div className={styles.rPContainer}>
-									<div className={styles.rPHead}>
-										<div className={styles.rPHContainer}>
-											<img className={styles.rPHImg} src="../../wp-content/2.png" />
-											
-										</div>
-									</div>
-
-									<div className={styles.rPBody}>
-										<h2>Ethereum moves Toward ETH 2.0 With 'Super Experiental' Testnet</h2>
-										<p>the fed is loss funds for israel-palestine conflict</p>
-										<div className={styles.rPBInfo}>
-											<b>Andrew Setyawan</b>
-											<time>at May 23, 2021</time>
-										</div>
-									</div>
-								</div>
-							</a>
-						</li>
-
-						<li className={styles.recommendPreview}>
-							<a href="#">
-								<div className={styles.rPContainer}>
-									<div className={styles.rPHead}>
-										<div className={styles.rPHContainer}>
-											<img className={styles.rPHImg} src="../../wp-content/3.jpg" />
-											
-										</div>
-									</div>
-
-									<div className={styles.rPBody}>
-										<h2>Apple Co-Founder Stave Wozniak's Lawsuit Over Youtube Bitcoin Scams Dismissed</h2>
-									</div>
-								</div>
-							</a>
-						</li>
-
-						<li className={styles.recommendPreview}>
-							<a href="#">
-								<div className={styles.rPContainer}>
-									<div className={styles.rPHead}>
-										<div className={styles.rPHContainer}>
-											<img className={styles.rPHImg} src="../../wp-content/4.jpg" />
-											
-										</div>
-									</div>
-
-									<div className={styles.rPBody}>
-										<h2>Hamas Sees Surge in Bitcoin Donations Amidst Israel-Palestine Conflict</h2>
-									</div>
-								</div>
-							</a>
-						</li>
-
-						<li className={styles.recommendPreview}>
-							<a href="#">
-								<div className={styles.rPContainer}>
-									<div className={styles.rPHead}>
-										<div className={styles.rPHContainer}>
-											<img className={styles.rPHImg} src="../../wp-content/5.jpg" />
-											
-										</div>
-									</div>
-
-									<div className={styles.rPBody}>
-										<h2>Paxful Pay Is an 'Invisible Bridge to Bitcoin' Says Cofounder</h2>
-									</div>
-								</div>
-							</a>
-						</li>
-
-						<li className={styles.recommendPreview}>
-							<a href="#">
-								<div className={styles.rPContainer}>
-									<div className={styles.rPHead}>
-										<div className={styles.rPHContainer}>
-											<img className={styles.rPHImg} src="../../wp-content/5.jpg" />
-											
-										</div>
-									</div>
-
-									<div className={styles.rPBody}>
-										<h2>Paxful Pay Is an 'Invisible Bridge to Bitcoin' Says Cofounder</h2>
-									</div>
-								</div>
-							</a>
-						</li>
-
-					</ul>
-				</div>
-
-				<div className={styles.learn}>
-					<h1 className={styles.contentTwoTitle}>Learn</h1>
-					<ul className={styles.learnContainer}>
-						<h3>this a content</h3>
-					</ul>
-				</div>
-			</div>
-
 		</main>
 	</>
 	)
