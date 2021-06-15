db51fd71f5acaf79ac99dc1df0f32abc80a40819
Andrew Setyawan <34556683+AndrewSich@users.noreply.github.com>
2021-06-15 18:59:27 +0700

    Update Main.jsx
    Update final popular content Main.jsx

--- components/main/Main.jsx
+++ components/main/Main.jsx
@@ -119,6 +119,23 @@ export const Main = () => {
 									</a>
 								</li>
 
+								<li className={styles.preview}>
+									<a href="#">
+										<div className={styles.previewHead}>
+											<img src="../../wp-content/2.png" alt="" />
+										</div>
+										<div className={styles.previewBody}>
+											<h2>Ethereum Moves Toward ETH 2.0 With 'Super Experimental' Testnet</h2>
+											<p>Bitcoin Is Cryptocurrency Based on C++ Engine Language Created By Satoshi Nakamoto On 2009 with Anouncement In StackOverFlow</p>
+											<div className={styles.pBDetail}>
+												<b>on Anime</b>
+												<time>2 minute ago</time>
+												<button className={styles.readBtn}>read</button>
+											</div>
+										</div>
+									</a>
+								</li>
+
 							</ul>
 						</div>
 					</div>
@@ -127,7 +144,226 @@ export const Main = () => {
 					<div className={styles.player}>
 							<button className={styles.button+" "+styles.play}></button>
 					</div>
-					<h2 className={styles.contentTitle}>Top Content</h2>
+					<div className={styles.top}>
+						<h2 className={styles.contentTitle}>Top Content</h2>
+						<ul className={styles.topContainer}>
+							
+							<li className={styles.topCard}>
+								<a href="#">
+									<div className={styles.tCC}>
+										<div className={styles.tCHead}>
+											<img src="../../wp-content/3.jpg" />
+										</div>
+										<div className={styles.tCBody}>
+											<h2>The Fed is Raises $300M For Buy Biologic Weapon From Israel</h2>
+											<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, seddo eiusmod tempor... incididunt ut labore et dolore magna aliqua.</p>
+											<div className={styles.tCBDetail}>
+												<b>on Cryptocurrency</b>
+												<time>7 minute ago</time>
+												<button className={styles.readBtn}>read</button>
+											</div>
+										</div>
+									</div>
+								</a>
+							</li>
+
+							<li className={styles.topCard}>
+								<a href="#">
+									<div className={styles.tCC}>
+										<div className={styles.tCHead}>
+											<img src="../../wp-content/4.jpg" />
+										</div>
+										<div className={styles.tCBody}>
+											<h2>Nvidia CEO: We're Moving Toward a Crypto Metaverse</h2>
+											<p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu... fugiat nulla pariatur</p>
+											<div className={styles.tCBDetail}>
+												<b>on Science</b>
+												<time>13 minute ago</time>
+												<button className={styles.readBtn}>read</button>
+											</div>
+										</div>
+									</div>
+								</a>
+							</li>
+
+							<li className={styles.topCard}>
+								<a href="#">
+									<div className={styles.tCC}>
+										<div className={styles.tCHead}>
+											<img src="../../wp-content/5.jpg" />
+										</div>
+										<div className={styles.tCBody}>
+											<h2>SEC Leaves Cryptocurrency Out of its Agenda</h2>
+											<p>The securrities and exchange commission (SEC) isn't too...</p>
+											<div className={styles.tCBDetail}>
+												<b>on Technology</b>
+												<time>35 minute ago</time>
+												<button className={styles.readBtn}>read</button>
+											</div>
+										</div>
+									</div>
+								</a>
+							</li>
+
+							<li className={styles.topCard}>
+								<a href="#">
+									<div className={styles.tCC}>
+										<div className={styles.tCHead}>
+											<img src="../../wp-content/2.png" />
+										</div>
+										<div className={styles.tCBody}>
+											<h2>Block.one Settles for $27.5 Million in EOS ICO Class-Action</h2>
+											<p>The securrities and exchange commission (SEC) isn't too...</p>
+											<div className={styles.tCBDetail}>
+												<b>on Technology</b>
+												<time>35 minute ago</time>
+												<button className={styles.readBtn}>read</button>
+											</div>
+										</div>
+									</div>
+								</a>
+							</li>
+
+							<li className={styles.topCard}>
+								<a href="#">
+									<div className={styles.tCC}>
+										<div className={styles.tCHead}>
+											<img src="../../wp-content/1.jpeg" />
+										</div>
+										<div className={styles.tCBody}>
+											<h2>Defi Tokens Including Aave, Uniswap, Compound Ride 'Musk Effect'</h2>
+											<p>The securrities and exchange commission (SEC) isn't too...</p>
+											<div className={styles.tCBDetail}>
+												<b>on Technology</b>
+												<time>35 minute ago</time>
+												<button className={styles.readBtn}>read</button>
+											</div>
+										</div>
+									</div>
+								</a>
+							</li>
+
+						</ul>
+					</div>
+				</div>
+			</div>
+			<div className={styles.mainContent}>
+				<div className={styles.mCL2}>
+					<div className={styles.popular}>
+						<h2 className={styles.contentTitle}>Popular Content</h2>
+						<div className={styles.cC}>
+							<ul className={styles.previewContainer}>
+
+								<li className={styles.preview+" "+styles.popularPreview}>
+									<a href="#">
+										<div className={styles.previewHead+" "+styles.pPHead}>
+											<img src="../../wp-content/2.png" />
+										</div>
+										<div className={styles.previewBody}>
+											<h2>Defi Tokens Including Aave, Uniswap Ride 'Musk Effect'</h2>
+											<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, seddo eiusmod tempor incididunt</p>
+											<div className={styles.pBDetail}>
+												<b>on Anime</b>
+												<time>13 minute ago</time>
+												<button className={styles.readBtn}>read</button>
+											</div>
+										</div>
+									</a>
+								</li>
+
+								<li className={styles.preview+" "+styles.popularPreview}>
+									<a href="#">
+										<div className={styles.previewHead+" "+styles.pPHead}>
+											<img src="../../wp-content/1.jpeg" />
+										</div>
+										<div className={styles.previewBody}>
+											<h2>The Chronical of Basic by Literals of Chronium Optical</h2>
+											<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, seddo eiusmod tempor incididunt</p>
+											<div className={styles.pBDetail}>
+												<b>on Technology</b>
+												<time>37 minute ago</time>
+												<button className={styles.readBtn}>read</button>
+											</div>
+										</div>
+									</a>
+								</li>
+
+								<li className={styles.preview+" "+styles.popularPreview}>
+									<a href="#">
+										<div className={styles.previewHead+" "+styles.pPHead}>
+											<img src="../../wp-content/3.jpg" />
+										</div>
+										<div className={styles.previewBody}>
+											<h2>Defi Tokens Including Aave, Uniswap Ride 'Musk Effect'</h2>
+											<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, seddo eiusmod tempor incididunt</p>
+											<div className={styles.pBDetail}>
+												<b>on Anime</b>
+												<time>13 minute ago</time>
+												<button className={styles.readBtn}>read</button>
+											</div>
+										</div>
+									</a>
+								</li>
+
+								<li className={styles.preview+" "+styles.popularPreview}>
+									<a href="#">
+										<div className={styles.previewHead+" "+styles.pPHead}>
+											<img src="../../wp-content/4.jpg" />
+										</div>
+										<div className={styles.previewBody}>
+											<h2>The Chronical of Basic by Literals of Chronium Optical</h2>
+											<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, seddo eiusmod tempor incididunt</p>
+											<div className={styles.pBDetail}>
+												<b>on Technology</b>
+												<time>37 minute ago</time>
+												<button className={styles.readBtn}>read</button>
+											</div>
+										</div>
+									</a>
+								</li>
+
+								<li className={styles.preview+" "+styles.popularPreview}>
+									<a href="#">
+										<div className={styles.previewHead+" "+styles.pPHead}>
+											<img src="../../wp-content/5.jpg" />
+										</div>
+										<div className={styles.previewBody}>
+											<h2>Defi Tokens Including Aave, Uniswap Ride 'Musk Effect'</h2>
+											<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, seddo eiusmod tempor incididunt</p>
+											<div className={styles.pBDetail}>
+												<b>on Anime</b>
+												<time>13 minute ago</time>
+												<button className={styles.readBtn}>read</button>
+											</div>
+										</div>
+									</a>
+								</li>
+
+								<li className={styles.preview+" "+styles.popularPreview}>
+									<a href="#">
+										<div className={styles.previewHead+" "+styles.pPHead}>
+											<img src="../../wp-content/1.jpeg" />
+										</div>
+										<div className={styles.previewBody}>
+											<h2>The Chronical of Basic by Literals of Chronium Optical</h2>
+											<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, seddo eiusmod tempor incididunt</p>
+											<div className={styles.pBDetail}>
+												<b>on Technology</b>
+												<time>37 minute ago</time>
+												<button className={styles.readBtn}>read</button>
+											</div>
+										</div>
+									</a>
+								</li>
+
+							</ul>
+						</div>
+					</div>
+				</div>
+				<div className={styles.mCR2}>
+					<div className={styles.learn}>
+						<h2 className={styles.contentTitle}>Learn Content</h2>
+					</div>
 				</div>
 			</div>
 		</main>
